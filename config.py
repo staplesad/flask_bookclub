227d0a8e8fe316e8e10a34db5be9773c2f2f5392
staplesad <staplesa@tcd.ie>
2017-06-12 21:23:45 +0100

    adding minimal config file - email is not functional

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+basedir= os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,
+'bookclub_app.db')
+
+WTF_CSRF_ENABLED=True
+SECRET_KEY='this-is-a-secret-key'
+
+ADMINS=['example@emailadress.com']
