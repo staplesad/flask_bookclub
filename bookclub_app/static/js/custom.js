1099c27141bc2ec38ad6f55384f408a713a86360
ruth-farrell <ruthcafarrell@gmail.com>
2017-06-13 15:26:41 +0100

    Confirm if people wanna sign out. Fixed Responsive table issue for nineteen eightyfour amen
    Also added a new button to the homepage.

--- bookclub_app/static/js/custom.js
+++ bookclub_app/static/js/custom.js
@@ -7,6 +7,13 @@ function updateCount(limit){
   $('#characters').text(output);
 }
 
+$(function(){
+    $('a#logout').click(function(){
+        if(confirm('Are you sure you want to sign out?')) {
+            return true;
+        }
 
-
+        return false;
+    });
+});
 
