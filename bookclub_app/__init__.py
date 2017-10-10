7a7d2f4b7c6c4123bffc5c120685e37ad41cab35
staplesad <staplesa@tcd.ie>
2017-10-10 15:16:30 +0100

    Updated database and database migration

--- bookclub_app/__init__.py
+++ bookclub_app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,5 +11,6 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 mail=Mail(app)
+migrate = Migrate(app, db)
 
 from bookclub_app import views, models
--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 basedir= os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,
-'bookclub_app.db')
-
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,
+#'bookclub_app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bookclub'
 WTF_CSRF_ENABLED=True
 SECRET_KEY='this-is-a-secret-key'
 
